b38b0bb47b386c1568445f528a0be38cc021bb09
Zetaphor <zetaphor@gmail.com>
2020-03-21 02:06:24 -0400

    Initial UI

---
+++ index.js
@@ -0,0 +1,14 @@
+// https://www.twitch.tv/popout/chap/chat?popout=
+
+document.addEventListener('DOMContentLoaded', function () {
+  let username = localStorage.getItem('username')
+  if (username !== null) document.getElementById('username').value = username
+})
+
+function openChat() {
+  let username = document.getElementById('username').value
+  if (!username.length) return
+  console.log('Open chat', username)
+  window.open(`https://www.twitch.tv/popout/${username}/chat?popout=`, '_blank')
+  localStorage.setItem('username', username)
+}
